f74314095dde088bc7ed78ee3eb067c42a15983b
010213pashaleshenko1-blip <010213pashaleshenko1@gmail.com>
2026-04-27 12:35:32 +0500

    Add game constants module

---
+++ src/game/constants.js
@@ -0,0 +1,27 @@
+export const MAP_W = 10000;
+export const MAP_H = 10000;
+export const PLAYER_SIZE = 34;
+export const BASE_TANK_RADIUS = 22;
+export const BULLET_RADIUS = 5;
+export const SPEED = 260;
+export const BOT_SPEED = 190;
+export const FIRE_COOLDOWN = 420;
+export const BOT_FIRE_COOLDOWN = 1100;
+export const PLAYER_TTL = 2500;
+export const JOYSTICK_RADIUS = 54;
+export const MAX_PLAYERS_PER_ROOM = 50;
+export const BOT_COUNT = 5;
+export const MAX_LEVEL = 25;
+export const ORBS_PER_KILL = 2;
+export const ROOMS = ['room-1', 'room-2', 'room-3', 'room-4', 'room-5'];
+
+export const BLOCKS = [
+  [780, 760, 520, 160], [1720, 1240, 220, 680], [2620, 820, 780, 210],
+  [4180, 1450, 320, 880], [5480, 900, 980, 220], [7240, 680, 260, 760],
+  [8480, 1450, 780, 240], [980, 3060, 900, 260], [2420, 2840, 260, 760],
+  [3520, 3360, 760, 240], [5200, 2860, 340, 980], [6820, 3240, 880, 260],
+  [8260, 2780, 320, 720], [1260, 5220, 700, 260], [2860, 4880, 320, 940],
+  [4200, 5480, 980, 260], [6200, 4940, 300, 760], [7700, 5360, 1000, 240],
+  [920, 7520, 800, 280], [2380, 6980, 280, 820], [3920, 7580, 760, 260],
+  [5660, 7020, 320, 860], [7220, 7440, 980, 260], [8780, 6940, 300, 900]
+];
