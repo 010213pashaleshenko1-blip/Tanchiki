080302d997668ac81e8d8988ae0c55281576fa3a
010213pashaleshenko1-blip <010213pashaleshenko1@gmail.com>
2026-04-27 20:19:11 +0500

    Switch entrypoint to GameV9

--- src/entry.jsx
+++ src/entry.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './GameV8.jsx';
+import App from './GameV9.jsx';
 import './styles.css';
 
 createRoot(document.getElementById('root')).render(<App />);
